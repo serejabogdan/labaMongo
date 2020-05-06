2de45097c1fc769c4af47fe133a1c329322f9d74
Hasst <sergey1112345@gmail.com>
2020-05-06 10:13:10 +0300

    labaMongo

--- client/js/definitions.js
+++ client/js/definitions.js
@@ -4,51 +4,14 @@ const $ = (tag) =>
 const $all = (tag) =>
   document.querySelectorAll(tag);
 
-// selects
-// получение селектора с ID titles
-const selectTitles = $("#titles");
-// получение селектора с ID teachers
-const selectTeachers = $("#teachers");
-// получение селектора с ID auditorium
-const selectAuditoriums = $("#auditorium");
-
-// table
-// получение таблицы
-const table = $("table");
-
 // buttons
 let buttonsMode = $all('.btn-mode');
 
 // blocks
 let dataTabs = $all('.data-tab');
 
-// button submit
-const submitBtn = $('.submit');
-
 // массив объектов кнопок и блоков
 const tabElements = getTabElements(buttonsMode, dataTabs);
-
-// adding-form inputs
-const postForm = $all('.post');
-const groupsInput = $('#group');
-const teachersInput = $('#teacher');
-
-// adding-form messages
-let messagesNodeList = $all('.list-group');
-const messages = getMessages(messagesNodeList);
-
-// функция формирования массива объектов кнопок и блоков
-function getMessages(messagesNodeList) {
-  const messages = [];
-  for(let message of messagesNodeList) {
-    messages.push({
-      name: message.title,
-      message
-    });
-  }
-  return messages;
-}
-messagesNodeList = null;
 
 // функция формирования массива объектов кнопок и блоков
 function getTabElements(buttonsMode, dataTabs) {
@@ -59,3 +22,17 @@ function getTabElements(buttonsMode, dataTabs) {
   return tabs;
 }
 dataTabs = buttonsMode = null;
+
+// ----next
+
+const groupSelect = $('.group-select');
+const groupResult = $('.group-result');
+
+const thingSelect = $('.thing-select');
+const teacherSelect = $('.teacher-select');
+const getLecturesBtn = $('.get-lecture');
+const lectureResult = $('.lecture-result');
+
+const getAditoriumsBtn = $('.auditoriums-btn');
+const auditoriumsResult = $('.auditoriums-result');
+
--- client/js/queries.js
+++ client/js/queries.js
@@ -10,98 +10,52 @@ function request(
   return XHR;
 }
 
-function getGroups() {
-  const XHR = request('../php/gets/groups.php');
+function getLabs(group) {
+  const XHR = request(`../php/gets.php?group=${group}`);
   XHR.onload = () => {
-    let result = XHR.response;
-    
-    selectTitles.innerHTML = `<option selected disabled>Выберите группу</option>`;
-    selectTitles.innerHTML += result;
-    groupsInput.innerHTML += result;
+    const result = JSON.parse(
+      XHR.response
+    );
+    printResult(groupResult, result);
   };
 }
+
+function getLectures(teacher, discipline) {
+    const XHR = request(`../php/gets.php?teacher=${teacher}&discipline=${discipline}`);
+    XHR.onload = () => {
+      const result = JSON.parse(
+        XHR.response
+      );
+      printResult(lectureResult, result);
+  };
+}
+
 function getAuditoriums() {
-  const XHR = request('../php/gets/teachers.php');
-  XHR.onload = () => {
-    let result = XHR.responseXML;
-    selectTeachers.innerHTML = `<option selected disabled>Выберите преподавателя</option>`;
-    for(let node of result.children[0].children) {
-      const htmlRow = `<option>${node.textContent}</option>`;
-      selectTeachers.innerHTML += htmlRow;
-      teachersInput.innerHTML += htmlRow;
+    const XHR = request(`../php/gets.php?auditorium=1`);
+    XHR.onload = () => {
+      let result = JSON.parse(
+        XHR.response
+      );
+      result = result.map(auditorium => ({auditorium: auditorium}));
+      printResult(auditoriumsResult, result);
+  };
+}
+
+function printResult(print, result) {
+    print.innerHTML = ``;
+    console.log(result)
+    if(result.length !== 0) {
+        for(let obj of result) {
+            print.innerHTML += `{<br/>`;
+                for(let key in obj) {
+                    if(key !== '_id')
+                    print.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;<b>${key}</b>: ${obj[key]}<br/>`;
+                }
+            print.innerHTML += `},<br/>`;
+        }
     }
-  };
-}
-function getTeachers() {
-  const XHR = request('../php/gets/auditoriums.php');
-  XHR.onload = () => {
-    let result = JSON.parse(
-      XHR.response
-    );
-    selectAuditoriums.innerHTML = `<option selected disabled>Выберите аудиторию</option>`;
-    result.forEach(item => selectAuditoriums.innerHTML += `<option>${item.auditorium}</option>`);
-  };
-}
-getGroups();
-getAuditoriums();
-getTeachers();
-
-function getLesson({ select, queryParam }) {
-  const url = `../php/gets/lesson.php?${queryParam}=${select.value}`;
-  const XHR = request(url); // вызов GET запроса
-  XHR.onload = () => { // onload callback
-    let result = JSON.parse(
-      XHR.response
-    );
-
-    clearHtmlElement(table);
-    createRow(result[0], "th");
-
-    for (let obj of result) {
-      createRow(obj, "td");
+    else {
+        print.innerHTML += `{<br/>`;
+        print.innerHTML += `}`;
     }
-  };
-  
-  // вызов функции сброса селектов
-  clearOtherSelects(select, [
-    selectTitles,
-    selectTeachers,
-    selectAuditoriums,
-  ]);
-}
-
-// создание строки в таблице
-function createRow(obj, typeCell) {
-  const tr = document.createElement( "tr" );
-  for (let propertyName in obj) {
-    const cell = document.createElement( typeCell );
-
-    if (typeCell === "th")
-      cell.innerHTML = propertyName;
-    else
-      cell.innerHTML =
-        obj[propertyName];
-
-    tr.append(cell);
-  }
-  table.append(tr);
-}
-
-// очистка html элемента
-function clearHtmlElement(element) {
-  element.innerHTML = "";
-}
-function clearHtmlElementsAll(elements) {
-  elements.forEach(element => element.innerHTML = '');
-}
-
-// функция сброса селектов
-function clearOtherSelects(
-  select,
-  allSelects
-) {
-  allSelects.forEach((item) => {
-    if (item !== select)
-      item.selectedIndex = 0;
-  });
 }
